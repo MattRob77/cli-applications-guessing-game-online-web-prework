8d644231db764d21e794642c1650c908c3a9a667
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-29 15:45:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,7 @@
 def run_guessing_game
   input = ""
-  number = rand(1...6) + 1 
+  number = rand(1...6) + 1
   puts "Guess a number between 1 and 6"
-  input = gets.chomp 
-
-  
+  input = gets.chomp
+end 
 end 
